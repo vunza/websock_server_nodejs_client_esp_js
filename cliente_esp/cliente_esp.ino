976772e73a4255183ceca317906807d5b33f8c35
vunza <vunza@yahoo.com.br>
2023-01-15 01:00:30 +0100

    Actualização do Servidor e Cliente ESP.

--- cliente_esp/cliente_esp.ino
+++ cliente_esp/cliente_esp.ino
@@ -1,38 +1,73 @@
 #include <Arduino.h>
-#include "WebSocketClient.h"
-#include "ESP8266WiFi.h"
+#include <ESP8266WiFi.h>
+#include <WebSocketsClient.h>
+#include <Hash.h>
 
-WebSocketClient ws(true);
+WebSocketsClient webSocket;
 
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      {
+        Serial.printf("[WSc] Connected to url: %s\n",  payload);
+        
+        // send message to server when Connected
+        webSocket.sendTXT("Connected");
+      }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send data to server
+      // webSocket.sendBIN(payload, length);
+      break;
+  }
+
+}
+
+
+/////////////
+// setup() //
+/////////////
 void setup() {
+
   Serial.begin(115200);
+  Serial.println();
+
   WiFi.begin("TPLINK", "gregorio@2012");
 
-  Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
+
     delay(500);
     Serial.print(".");
   }
 
-   
-   Serial.println("\nConnected : ");
+  //WiFi connected, IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
   Serial.println(WiFi.localIP());
+  
+  webSocket.beginSSL("ws-server-client-esp-js.onrender.com", 443);
+  webSocket.onEvent(webSocketEvent);
+
 }
 
-void loop() {
-  
-  if (!ws.isConnected()) {
-    Serial.println("Connectting...");
-    ws.connect("servidor-nodejs.onrender.com", "/", 443);    
-  } else {
-    ws.send("hello");
-    Serial.println("Conectedo.");
 
-    String msg;
-    if (ws.getMessage(msg)) {
-      Serial.println(msg);
-    }
-  }
-  
-  delay(500);
+////////////
+// loop() //
+////////////
+void loop() {
+  webSocket.loop();
 }
